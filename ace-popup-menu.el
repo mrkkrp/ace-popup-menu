e99f92bb1e6ef12b88be7679617b5da813c5b4d7
mrkkrp <markkarpov@opmbx.org>
2015-07-08 23:10:02 +0600

    reorganize code, add customization variables

--- ace-popup-menu.el
+++ ace-popup-menu.el
@@ -36,6 +36,88 @@
 (require 'avy)
 (require 'cl-lib)
 
+(defgroup ace-popup-menu nil
+  "Replace GUI popup menu with something more efficient."
+  :group 'convenience
+  :prefix "ace-popup-menu")
+
+;;;###autoload
+(define-minor-mode ace-popup-menu-mode
+  "Toggle ace-popup-menu-mode minor mode.
+With a prefix argument ARG, enable ace-popup-menu mode if ARG is
+positive, and disable it otherwise.  If called from Lisp, enable
+the mode if ARG is omitted or nil, and toggle it if ARG is
+`toggle'.
+
+This minor mode is global. When it's active any call to
+`x-popup-menu' will result in call of `ace-popup-menu'
+instead. This function in turn implements more efficient
+interface to select an option from a list. Emacs Lisp code can
+also use `ace-popup-menu' directly, in this case it will work OK
+even if the mode is disabled."
+  :global t
+  :group 'ace-popup-menu
+  (if ace-popup-menu-mode
+      (advice-add 'x-popup-menu :override #'ace-popup-menu)
+    (advice-remove 'x-popup-menu #'ace-popup-menu)))
+
+(defcustom ace-popup-menu-show-pane-header t
+  "Whether to print headers of individual panes in Ace Popup Menu."
+  :type 'boolean
+  :group 'ace-popup-menu)
+
+;;;###autoload
+(defun ace-popup-menu (position menu)
+  "Pop up menu in a temporary window and return user's selection.
+Argument POSITION is taken for compatibility and ignored unless
+it's NIL, in this case this function has no effect.  For meaning
+of MENU argument see description of `x-popup-menu'.
+
+Every selectable item in the menu is labeled with a letter (or
+two).  User can press letter corresponding to desired menu item
+and he is done."
+  (when position
+    (let ((buffer (get-buffer-create "*Ace Popup Menu*"))
+          menu-item-alist
+          (first-pane t))
+      (with-displayed-buffer-window
+       ;; buffer or name
+       buffer
+       ;; action (for `display-buffer')
+       (cons 'display-buffer-below-selected
+             '((window-height . fit-window-to-buffer)
+               (preserve-size . (nil . t))))
+       ;; quit-function
+       (lambda (window _value)
+         (with-selected-window window
+           (unwind-protect
+               (cdr
+                (assq
+                 (avy--with-avy-keys ace-popup-menu
+                   (avy--process (mapcar #'car menu-item-alist)
+                                 #'avy--overlay-pre))
+                 menu-item-alist))
+             (when (window-live-p window)
+               (quit-restore-window window 'kill)))))
+       ;; Here we generate the menu. Currently MENU cannot be a keymap or
+       ;; list of keymaps. Support for this representation of MENU will be
+       ;; added on request later.
+       (with-current-buffer buffer
+         (cl-destructuring-bind (title . panes) menu
+           (insert (propertize title 'face 'font-lock-function-name-face)
+                   "\n\n")
+           (dolist (pane panes)
+             (cl-destructuring-bind (title . items) pane
+               (if first-pane
+                   (setq first-pane nil)
+                 (insert "\n\n"))
+               (when ace-popup-menu-show-pane-header
+                 (insert (propertize title 'face 'underline)
+                         "\n\n"))
+               (setq menu-item-alist
+                     (append (ace-popup-menu--insert-strings items)
+                             menu-item-alist))))))))))
+
 (defun ace-popup-menu--insert-strings (items)
   "Insert ITEMS much like `completion--insert-strings' in current buffer.
 
@@ -86,77 +168,6 @@ value according to ITEMS."
             (setq column (+ column
                             (* colwidth (ceiling length colwidth)))))))
       result)))
-
-;;;###autoload
-(defun ace-popup-menu (position menu)
-  "Pop up menu in a temporary window and return user's selection.
-Argument POSITION is taken for compatibility and ignored unless
-it's NIL, in this case this function has no effect.  For meaning
-of MENU argument see description of `x-popup-menu'.
-
-Every selectable item in the menu is labeled with a letter (or
-two).  User can press letter corresponding to desired menu item
-and he is done."
-  (when position
-    (let ((buffer (get-buffer-create "*Ace Popup Menu*"))
-          menu-item-alist
-          (first-pane t))
-      (with-displayed-buffer-window
-       ;; buffer or name
-       buffer
-       ;; action (for `display-buffer')
-       (cons 'display-buffer-below-selected
-             '((window-height . fit-window-to-buffer)
-               (preserve-size . (nil . t))))
-       ;; quit-function
-       (lambda (window _value)
-         (with-selected-window window
-           (unwind-protect
-               (cdr
-                (assq
-                 (avy--with-avy-keys ace-popup-menu
-                   (avy--process (mapcar #'car menu-item-alist)
-                                 #'avy--overlay-pre))
-                 menu-item-alist))
-             (when (window-live-p window)
-               (quit-restore-window window 'kill)))))
-       ;; Here we generate the menu. Currently MENU cannot be a keymap or
-       ;; list of keymaps. Support for this representation of MENU will be
-       ;; added on request later.
-       (with-current-buffer buffer
-         (cl-destructuring-bind (title . panes) menu
-           (insert (propertize title 'face 'font-lock-function-name-face)
-                   "\n\n")
-           (dolist (pane panes)
-             (cl-destructuring-bind (title . items) pane
-               (if first-pane
-                   (setq first-pane nil)
-                 (insert "\n\n"))
-               (insert (propertize title 'face 'underline)
-                       "\n\n")
-               (setq menu-item-alist
-                     (append (ace-popup-menu--insert-strings items)
-                             menu-item-alist))))))))))
-
-;;;###autoload
-(define-minor-mode ace-popup-menu-mode
-  "Toggle ace-popup-menu-mode minor mode.
-With a prefix argument ARG, enable ace-popup-menu mode if ARG is
-positive, and disable it otherwise.  If called from Lisp, enable
-the mode if ARG is omitted or nil, and toggle it if ARG is
-`toggle'.
-
-This minor mode is global. When it's active any call to
-`x-popup-menu' will result in call of `ace-popup-menu'
-instead. This function in turn implements more efficient
-interface to select an option from a list. Emacs Lisp code can
-also use `ace-popup-menu' directly, in this case it will work OK
-even if the mode is disabled."
-  :global t
-  :group 'ace-popup-menu
-  (if ace-popup-menu-mode
-      (advice-add 'x-popup-menu :override #'ace-popup-menu)
-    (advice-remove 'x-popup-menu #'ace-popup-menu)))
 
 (provide 'ace-popup-menu)
 

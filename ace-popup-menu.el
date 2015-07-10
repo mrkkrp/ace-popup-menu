b2352c894f9183f564f4279086a80639635fc32b
mrkkrp <markkarpov@opmbx.org>
2015-07-10 12:49:01 +0600

    fixing home page of the package

--- ace-popup-menu.el
+++ ace-popup-menu.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2015 Mark Karpov <markkarpov@openmailbox.org>
 ;;
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
-;; URL: https://github.com/ace-popup-menu
+;; URL: https://github.com/mrkkrp/ace-popup-menu
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5") (avy "0.2.0"))
 ;; Keywords: convenience, popup, menu

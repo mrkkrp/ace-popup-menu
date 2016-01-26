3e771b470b0c633d7633dceec054fc05beac81f0
mrkkrp <markkarpov@opmbx.org>
2016-01-26 18:31:11 +0600

    Lower the minimal version of Emacs

--- ace-popup-menu.el
+++ ace-popup-menu.el
@@ -5,7 +5,7 @@
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/mrkkrp/ace-popup-menu
 ;; Version: 0.2.0
-;; Package-Requires: ((emacs "24.4") (avy-menu "0.1"))
+;; Package-Requires: ((emacs "24.3") (avy-menu "0.1"))
 ;; Keywords: convenience, popup, menu
 ;;
 ;; This file is not part of GNU Emacs.
